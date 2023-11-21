1915aa42615ea41a0efb9c8b655d1a83fe6b3cb2
DaniilDaniil <daniil.eudokimou@gmail.com>
2023-11-21 09:54:50 +0300

    after refactor ver 1.0.0 add read.me

--- install/assets/index.js
+++ install/assets/index.js
@@ -1,39 +1,42 @@
-function isSet (n){
+function isSet(n) {
     if (n !== undefined && n !== null) {
         return true;
     }
 }
+
 function trimFilename(path) {
     return path.substring(path.lastIndexOf("/") + 1);
 }
- function appendProgressBartHtml (percent = 0){
+
+function appendProgressBartHtml(percent = 0) {
     let progressBarContainer = document.querySelector('.progress-bar__wrapper')
-    if(isSet(progressBarContainer)){
+    if (isSet(progressBarContainer)) {
         progressBarContainer.remove();
         $("#edit_edit_table").parent().append(function (i) {
             return "<div class=\"progress-bar__wrapper container\">\n" +
                 "      <span class=\"progress-bar__text\">Загрузка:</span>\n" +
                 "  <div class=\"progress-bar__container\">\n" +
-                "    <div class=\"progress-bar\" style=\"left: " + (-100 + (percent)) +"%\" >\n" +
+                "    <div class=\"progress-bar\" style=\"left: " + (-100 + (percent)) + "%\" >\n" +
                 "    </div>\n" +
                 "  </div>\n" +
                 "</div>";
         });
-    }else{
+    } else {
         $("#edit_edit_table").parent().append(function (i) {
             return "<div class=\"progress-bar__wrapper container\">\n" +
                 "      <span class=\"progress-bar__text\">Загрузка:</span>\n" +
                 "  <div class=\"progress-bar__container\">\n" +
-                "    <div class=\"progress-bar\" style=\"left: " + (-100 + (percent))  +"%\" >\n" +
+                "    <div class=\"progress-bar\" style=\"left: " + (-100 + (percent)) + "%\" >\n" +
                 "    </div>\n" +
                 "  </div>\n" +
                 "</div>";
         });
     }
 }
-function appendResultContainerHtml (findedCount, convertedCount){
+
+function appendResultContainerHtml(findedCount, convertedCount) {
     let resultContainer = document.querySelector('.result__wrapper')
-    if(isSet(resultContainer)) {
+    if (isSet(resultContainer)) {
         resultContainer.remove();
         $("#edit_edit_table").parent().append(function (i) {
             return "<div class=\"result__wrapper container\">\n" +
@@ -47,7 +50,7 @@ function appendResultContainerHtml (findedCount, convertedCount){
                 "  </ul>\n" +
                 "</div>";
         });
-    }else{
+    } else {
         $("#edit_edit_table").parent().append(function (i) {
             return "<div class=\"result__wrapper container\">\n" +
                 "      <label class=\"result__checkbox-wrapper\">Отображать только названия файлов" +
@@ -62,28 +65,30 @@ function appendResultContainerHtml (findedCount, convertedCount){
         });
     }
 }
-function appendResultHtml(arr){
-    if(arr.length > 5000) {
+
+function appendResultHtml(arr) {
+    if (arr.length > 5000) {
         $(".result__list").append(function (i) {
             return "<li class=\"result__list-item\">Слишком большое кол-во файлов</li>\n"
         });
-    }else{
+    } else {
         for (const [key, value] of Object.entries(arr)) {
-            if(key !== 'success'){
+            if (key !== 'success') {
                 $(".result__list").append(function (i) {
-                    return "<li class=\"result__list-item\" data-toggled=\"false\" data-file-path='" + value + "' data-file-name=\n"+  trimFilename(value) +" >\n" + value + "</li>\n"
+                    return "<li class=\"result__list-item\" data-toggled=\"false\" data-file-path='" + value + "' data-file-name=\n" + trimFilename(value) + " >\n" + value + "</li>\n"
                 });
             }
         }
     }
 }
-function handleFilesNames(){
+
+function handleFilesNames() {
     $(document).on('click', '.checkmark', function (e) {
         $('.result__list-item').each(function (index) {
-            if($(this).attr('data-toggled') === 'true'){
+            if ($(this).attr('data-toggled') === 'true') {
                 $(this).text($(this).attr('data-file-path'));
                 $(this).attr('data-toggled', false)
-            }else{
+            } else {
                 $(this).text($(this).attr('data-file-name'));
                 $(this).attr('data-toggled', true)
             }
@@ -91,31 +96,34 @@ function handleFilesNames(){
 
     })
 }
+
 function calculatePercentage(total, available) {
     if (total === 0) {
         return 0; // чтобы избежать деления на ноль
     }
     return (available / total) * 100;
 }
-function handleButtonLoader (button) {
+
+function handleButtonLoader(button) {
     button.classList.remove('adm-btn-load')
     button.removeAttribute('disabled')
     $('.adm-btn-load-img-green').remove();
 }
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
     if (!Object.prototype.length) {
         Object.defineProperty(Object.prototype, 'length', {
-            get: function() {
+            get: function () {
                 return Object.keys(this).length
             }
         })
     }
     let convertForm = document.querySelector('.form__convert');
     let convertButton = document.querySelector('.adm-btn-save');
-    convertForm.addEventListener('submit', function (e){
+    convertForm.addEventListener('submit', function (e) {
         e.preventDefault();
         let responseWrapper = document.querySelector('.files-list-wrapper');
-        if(isSet(responseWrapper)) {
+        if (isSet(responseWrapper)) {
             $(".files-list-wrapper").remove();
         }
         var data = $('.form__convert').serializeArray();
@@ -124,51 +132,65 @@ document.addEventListener("DOMContentLoaded", function() {
             method: 'post',
             data: data,
             dataType: 'json',
-            success: function(data){
+            success: function (data) {
                 let response = data;
-                if(isSet(response.success)){
-                    let upperCounter = parseInt((response.length/100) * 5)
-                    if(upperCounter === 0){
-                        upperCounter = 1;
-                    }
-                    let c = 0;
-                    for(let i = 0; i < response.length -1; i = i + upperCounter){
-                        let counter = i + upperCounter;
-                        let iterationData = [];
-                        for (const [key, value] of Object.entries(response)) {
-                                if((parseInt(key) < counter) && (i <= parseInt(key))){
-                                iterationData.push(value);
-                            }
+                if (isSet(response.success)) {
+                    function runAllAjax(array) {
+                        var i = 0;
+                        var convertedCount = 0;
+                        let upperCounter = 2;
+                        if(array.length > 100){
+                            upperCounter = 50;
                         }
-                        $('.form-ajax__action').attr('name', 'ajax_convert_files');
-                        $('.form-ajax__action').val(iterationData)
-                        iterationData = $('.form__convert').serializeArray();
-
-                        $.ajax({
-                            url: '',
-                            method: 'post',
-                            data: iterationData,
-                            dataType: 'json',
-                            success: function (res){
-                                c = c + res.length
-                                appendResultContainerHtml(response.length - 1, c);
-                                appendResultHtml(response);
-                                handleFilesNames();
-                                appendProgressBartHtml(calculatePercentage(response.length - 1, c))
-                                if((response.length - 1) === c) {
-                                    handleButtonLoader(convertButton) // убираем лоадер с кнопки
+                        if(array.length > 1000){
+                            upperCounter = 100;
+                        }
+                        if(array.length > 10000){
+                            upperCounter = 300;
+                        }
+                        function next() {
+                            let iterationData = [];
+                            for (const [key, value] of Object.entries(response)) {
+                                if ((parseInt(key) < (i + upperCounter)) && (i <= parseInt(key))) {
+                                    iterationData.push(value);
                                 }
                             }
-                        })
+                            $('.form-ajax__action').attr('name', 'ajax_convert_files');
+                            $('.form-ajax__action').val(iterationData)
+                            iterationData = $('.form__convert').serializeArray();
+                            $.ajax({
+                                async: true,
+                                method: 'post',
+                                url: '',
+                                data: iterationData,
+                                dataType: 'json',
+                                success: function (res) {
+                                    convertedCount = convertedCount + res.length;
+                                    i = i + upperCounter
+                                    if (i >= array.length - 1) {
+                                        appendResultContainerHtml(array.length - 1, convertedCount);
+                                        appendResultHtml(array);
+                                        handleFilesNames();
+                                        appendProgressBartHtml(calculatePercentage(array.length - 1, convertedCount))
+                                        handleButtonLoader(convertButton) // убираем лоадер с кнопки
+                                        $('.form-ajax__action').attr('name', 'ajax_find_files');
+                                        $('.form-ajax__action').val(1)
+                                    } else {
+                                        appendResultContainerHtml(array.length - 1, convertedCount);
+                                        handleFilesNames();
+                                        appendResultHtml(array);
+                                        appendProgressBartHtml(calculatePercentage(array.length - 1, convertedCount))
+                                        next();
+                                    }
+                                }
+                            });
+                        }
+                        next();
                     }
-                    $('.form-ajax__action').attr('name', 'ajax_find_files');
-                    $('.form-ajax__action').val(1)
+                    runAllAjax(response)
 
                 }
             }
         });
     })
-
-
-
 });
--- lib/Main.php
+++ lib/Main.php
@@ -7,39 +7,6 @@ namespace WebpConvert\Main;
 class Main
 {
     function __construct(){}
-
-    public function appendJavaScriptAndCSS() {
-
-        // прокрутка будет работать только в публичной части
-        if (!defined('ADMIN_SECTION') && ADMIN_SECTION !== true) {
-            $module_id = pathinfo(dirname(__DIR__))['basename'];
-            $options = json_encode(
-                array(
-                    'switch_on'     => Option::get($module_id, 'switch_on', 'Y'),
-                    'width'         => Option::get($module_id, 'width', '50'),
-                    'height'        => Option::get($module_id, 'height', '50'),
-                    'radius'        => Option::get($module_id, 'radius', '50'),
-                    'color'         => Option::get($module_id, 'color', '#bf3030'),
-                    'side'          => Option::get($module_id, 'side', 'left'),
-                    'indent_bottom' => Option::get($module_id, 'indent_bottom', '10'),
-                    'indent_side'   => Option::get($module_id, 'indent_side', '10'),
-                    'speed'         => Option::get($module_id, 'speed', 'normal')
-                )
-            );
-            Asset::getInstance()->addCss('/bitrix/css/'.$module_id.'/style.css');
-            // подключить библиотеку jQuery?
-            if (Option::get($module_id, 'jquery_on', 'N') == 'Y') {
-                \CJSCore::init(array('jquery2'));
-            }
-            Asset::getInstance()->addString(
-                "<script id='".$module_id."-params' data-params='".$options."'></script>",
-                true
-            );
-            Asset::getInstance()->addJs('/bitrix/js/'.$module_id.'/script.js');
-
-        }
-    }
-
     public static function webpConvert($file, $compression_quality = 80){
         if (!file_exists($file)) {
             return false;
